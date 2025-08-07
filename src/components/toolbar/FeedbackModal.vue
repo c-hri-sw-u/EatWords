1b2a7a227eb7fc51aec6aecca01ade5246e28ef6
Chris <cwu14932@gmail.com>
2025-08-07 01:06:52 -0400

    5555

--- src/components/toolbar/FeedbackModal.vue
+++ src/components/toolbar/FeedbackModal.vue
@@ -15,7 +15,7 @@ const emit = defineEmits([
       title="反馈">
     <div class="feedback-modal">
       <div>
-        给我发Email：<a href="mailto:zyronon@163.com">zyronon@163.com</a>
+        给我发Email：<a href="mailto:cwu14932@gmail.com">cwu14932@gmail.com</a>
       </div>
       <p>or</p>
       <div class="github">
